4c1c93e551968c66b395d4101d593e5d921cc5cc
LiarZishan <LiarZishan@LiarZishan>
2020-02-27 02:48:07 +0600

    27-02-2020

---
+++ basicWebDriverImplement/src/basicWebDriverImplement/basicDriverImpl.java
@@ -0,0 +1,22 @@
+package basicWebDriverImplement;
+
+import com.openqa.selenium.WebDriver;
+import com.openqa.selenium.chrome.ChromeDriver;
+import com.openqa.selenium.firefox.FirefoxDriver;
+/*
+ * Here Import a zishanDriver003.jar files (custom created) to support WebDriver
+ * You Find this jar file information from basicWebDriver java project which later export as .jar and
+ * import in this project
+ */
+public class basicDriverImpl 
+{
+	public static void main(String[] args) 
+	{
+		FirefoxDriver firefox = new FirefoxDriver();
+		firefox.click("username");
+		
+		
+		WebDriver driver = new ChromeDriver();
+		driver.sendKeys("Password", "123");
+	}
+}
