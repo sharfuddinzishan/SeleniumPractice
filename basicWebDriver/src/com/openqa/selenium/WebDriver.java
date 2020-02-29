0294e7f452572516c2631aa264cf93e4f7ff16c2
Sharfuddin <LiarZishan@LiarZishan>
2020-03-01 00:24:24 +0600

    01-March-2020

--- basicWebDriver/src/com/openqa/selenium/WebDriver.java
+++ basicWebDriver/src/com/openqa/selenium/WebDriver.java
@@ -1,5 +1,9 @@
 package com.openqa.selenium;
-
+/*
+ * 
+ * basicWebDriver whole package export as .jar and add this .jar in another project build path
+ * 
+ */
 public interface WebDriver 
 {
 	public void click(String location);
