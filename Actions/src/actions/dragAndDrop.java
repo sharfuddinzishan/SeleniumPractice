6ba4cb51eb817e2c7f7fe43ea4ee3a9701e91876
sharfuddin <sharfuddin.cse@gmail.com>
2020-03-28 01:56:43 +0600

    28-March-2020

--- Actions/src/actions/dragAndDrop.java
+++ Actions/src/actions/dragAndDrop.java
@@ -4,15 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-public class actionsEvent {
+public class dragAndDrop {
 	public static WebDriver driver=null;
 	public static WebElement btn = null;
 
@@ -30,27 +29,17 @@ public class actionsEvent {
 		option.setBinary("C:\\Users\\LiarZishan\\AppData\\Local\\Chromium\\Application\\chrome.exe");
 		driver = new ChromeDriver();
 		
-        driver.get("https://www.jqueryscript.net/demo/drag-drop-image-uploader/");
+        driver.get("https://jquery-drag-drop-demo.webflow.io");
         driver.manage().window().maximize();
 		
-        By pageDown = By.xpath("//*[@id='form-example-2']/button");
-        driver.findElement(pageDown).sendKeys(Keys.PAGE_DOWN);
-		Thread.sleep(2000);
-		
 		Actions acts = new Actions(driver);
-        By name = By.id("name-2");
-        WebElement doubleClick = driver.findElement(name);
-		Action act=acts.moveToElement(doubleClick).doubleClick().keyDown(Keys.CONTROL).sendKeys("a").
-		keyUp(Keys.CONTROL).keyDown(Keys.SHIFT).sendKeys("liar Zishan").keyUp(Keys.SHIFT).build();
-		act.perform();
-		Thread.sleep(2000);
 		
-		WebElement image1 = driver.findElement(By.xpath("//img[@src='https://picsum.photos/500/500?random=1']"));
-		WebElement image1Click = driver.findElement(By.xpath("//img[@src='https://picsum.photos/500/500?random=1']//following::button[1]//i"));
-		acts.moveToElement(image1).click().click(image1Click).build().perform();
-		Thread.sleep(2000);
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement target = driver.findElement(By.id("droppable"));
 		
-		acts.contextClick(image1).moveByOffset(100,100).build().perform();
+		acts.dragAndDrop(source, target).build().perform();
+		Thread.sleep(1500);
+		acts.clickAndHold(source).moveToElement(target,-10,-10).build().perform();
 		
 
 		Thread.sleep(5000);
